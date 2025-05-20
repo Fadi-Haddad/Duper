ba9a55ee9b61426091ba49b2f4be010a737051af
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2025-05-21 00:04:53 +0300

    Defined init and a function to handle :next_path call

--- lib/duper/path_finder.ex
+++ lib/duper/path_finder.ex
@@ -2,7 +2,13 @@ defmodule Duper.PathFinder do
 
   use GenServer
 
+## Implementation
   def init(path) do
     DirWalker.start_link(path)
   end
+  def handle_call({:next_path}, _from, dir_walker) do
+    path = dir_walker.next(dir_walker)
+    {:reply, path, dir_walker}
+  end
+
 end
