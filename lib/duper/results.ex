cd72c3bbdd07e7dc4cd4a2f0cabf5a7a2b3a2a21
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2025-05-20 21:44:25 +0300

    Defined a function to filter the hash:paths map

--- lib/duper/results.ex
+++ lib/duper/results.ex
@@ -4,9 +4,14 @@ defmodule Duper.Results do
   def init(_) do
     {:ok, %{}}
   end
+
   def handle_call(:find_duplicates, _from, results) do
     {:reply, hashes_with_more_than_one_path(results), results}
   end
 
-
+  defp hashes_with_more_than_one_path(results) do
+    results
+    |> Enum.filter(fn {_hash, path_list} -> length(path_list) >=2 end)
+    |> Enum.map(fn {hash, _path_list} -> hash end)
+  end
 end
