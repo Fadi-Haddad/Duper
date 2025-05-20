294125fd6bff87388146af5155190c2b606c330e
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2025-05-21 00:31:22 +0300

    Added an Interface to PathFinder GenServer

--- lib/duper/path_finder.ex
+++ lib/duper/path_finder.ex
@@ -1,13 +1,22 @@
 defmodule Duper.PathFinder do
 
   use GenServer
+  @me __MODULE__
+
+## Interface
+  def start_link(root) do
+    GenServer.start_link(__MODULE__, root, name: @me)
+  end
+  def next_path() do
+    GenServer.call(@me, {:next_path})
+  end
 
 ## Implementation
   def init(path) do
     DirWalker.start_link(path)
   end
   def handle_call({:next_path}, _from, dir_walker) do
-    path = dir_walker.next(dir_walker)
+    path = DirWalker.next(dir_walker)
     {:reply, path, dir_walker}
   end
 
